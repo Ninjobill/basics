5d614dcbb6f87b88c9b6400507ad946395e2966c
Billy Todd <billy.todd50@yahoo.com>
2019-08-01 21:39:34 -0700

    ternarys and switch

--- script.js
+++ script.js
@@ -169,13 +169,54 @@
 // *******************
 // Boolean logic
 
-var firstName = 'john';
-var age = 16;
+// var firstName = 'john';
+// var age = 16;
 
-if (age < 13) {
-    console.log(firstName + ' is a boy.');
-} else if (age > 13 && age < 20) { // between 13 and 20
-    console.log(firstName + ' is a teenager.');
-} else { // 20 or older
-    console.log(firstName + ' is a man.');
+// if (age < 13) {
+//     console.log(firstName + ' is a boy.');
+// } else if (age > 13 && age < 20) { // between 13 and 20
+//     console.log(firstName + ' is a teenager.');
+// } else { // 20 or older
+//     console.log(firstName + ' is a man.');
+// }
+
+// *******************
+// the Ternary Operator and Switch Statements
+
+var firstName = 'John';
+var age = 20;
+
+// age >= 18 ? console.log(firstName + ' drinks beer.') 
+// : console.log(firstName + ' drinks juice.')
+
+// var drink = age >= 18 ? 'beer' : 'juice';
+// console.log(drink);
+
+// switch statement
+
+var job = 'instructor';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+            console.log(firstName + ' is a teenager.');
+            break;
+    default:
+            console.log(firstName + ' is a man.');
 }
