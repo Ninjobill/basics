a01fc32a669514eabec1534bf86465c6faca1fc7
Billy Todd <billy.todd50@yahoo.com>
2019-08-01 20:01:54 -0700

    precedence, assignments, operators

--- script.js
+++ script.js
@@ -53,28 +53,64 @@
 // basic operators
 // ********************
 
-var year, yearJohn, yearMark;
-now = 2018;
-ageJohn = 28;
-ageMark = 33;
-// math operators
-yearJohn = now - ageJohn;
-yeahMark = now - ageMark;
-console.log(yearJohn);
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
+// // math operators
+// yearJohn = now - ageJohn;
+// yeahMark = now - ageMark;
+// console.log(yearJohn);
 
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
 
-// logical operators
+// // logical operators
 
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder);
 
-// typeof operator
+// // typeof operator
 
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof 'mark');
-var x;
-console.log(typeof x);
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'mark');
+// var x;
+// console.log(typeof x);
+
+// ********************
+// Operator precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// multiple operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+// grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// multiple assignments
+var x, y;
+
+x = y = ( 3 + 5 ) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// more operators
+
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+
+x = x + 1;
+x += 1;
+x++;
+x--;
+console.log(x);
